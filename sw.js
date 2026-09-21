9408b872aceb9d07f18103bc68b9772205e4319b
Xavia <Chideramaduka2007@gmail.com>
2026-09-21 22:41:45 +0100

    Optimize 1024px tablet navbar spacing and category pill centering

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@
  * Cache-first strategy for static assets, network-first for HTML/data
  */
 
-const CACHE_NAME = 'melojey-v13';
-const STATIC_CACHE = 'melojey-static-v13';
-const DATA_CACHE = 'melojey-data-v13';
+const CACHE_NAME = 'melojey-v14';
+const STATIC_CACHE = 'melojey-static-v14';
+const DATA_CACHE = 'melojey-data-v14';
 
 const STATIC_ASSETS = [
   '/',
