da024a41ff911918d824d07d01e048389fc1506f
Xavia <Chideramaduka2007@gmail.com>
2026-09-21 17:33:25 +0100

    Align mobile search icon to the right beside wishlist and cart

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@
  * Cache-first strategy for static assets, network-first for HTML/data
  */
 
-const CACHE_NAME = 'melojey-v11';
-const STATIC_CACHE = 'melojey-static-v11';
-const DATA_CACHE = 'melojey-data-v11';
+const CACHE_NAME = 'melojey-v12';
+const STATIC_CACHE = 'melojey-static-v12';
+const DATA_CACHE = 'melojey-data-v12';
 
 const STATIC_ASSETS = [
   '/',
